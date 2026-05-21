3e81e1e144b7267c1a68705d0e303ee38596bf76
Mal <240441674+rdf-mal@users.noreply.github.com>
2026-05-21 10:50:08 +0200

    Add initial functions for electrical calculations

---
+++ main.py
@@ -0,0 +1,24 @@
+# Funktion 1
+def strom_berechnen(leistung, spannung):
+    # TODO
+
+# Funktion 2
+def spannungsfall(strom, widerstand):
+    # Todo
+
+# Funktion 3
+def verlustleistung(strom, widerstand):
+    # TODO
+
+
+leistung = 2000   # Watt
+spannung = 230    # Volt
+widerstand = 0.5  # Ohm
+
+strom = strom_berechnen(leistung, spannung)
+u_verlust = spannungsfall(strom, widerstand)
+p_verlust = verlustleistung(strom, widerstand)
+
+print("Strom:", round(strom, 2), "A")
+print("Spannungsfall:", round(u_verlust, 2), "V")
+print("Verlustleistung:", round(p_verlust, 2), "W")
